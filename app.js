473b928a2ee64e0e96a4036275500409b3e5c27f
Jamie Yamashita <jamieyamashita@Jamies-MacBook-Pro-2.local>
2019-01-26 11:43:21 -1000

    got branches to work

--- app.js
+++ app.js
@@ -11,17 +11,12 @@ let goat;
 
 let audio = new Audio('/assets/goatscream.mp3');
 
-// const colors = ['#4deeea', '#74ee15', '#ffe700', '#f000ff', '#001eff', '#ff0303', '#8400ff', '#00fff6', '#0028ff', '#00ff28', '#ffa300', '#cf0060', '#ff00ff', '#13a8fe', '#4e87a4', '#b0d5ce', '#fff1e4', '#fa86ab', '#ee2889','#7b297d', '#e87888', '#eae8e5', '#b1185a','#c351a2', '#efa9df', '#f3cff1']
-
-// let goatsArray = [];
-
-// branches
 let branches = [
   {x1: 200, x2: 400, y: 300},
   {x1: 500, x2: 700, y: 400},
   {x1: 100, x2: 300, y: 500},
   {x1: 600, x2: 800, y: 600},
-  {x1: 50, x2: 250, y: 700}
+  {x1: 50, x2: 250, y: 700},
 ]
 
 // pairs of x,y coordinates, function that generates the branches and also defines the areas where the goat stops
@@ -93,40 +88,22 @@ function Goat (x, y, dx, dy, height, width) {
   this.draw = function(){
     // console.log('drawing');
 
-
-
-    // let goatX = this.x;
-    // let goatY = this.y;
-    // let goatH = 100;
-    // let goatW = 100;
-
     ctx.globalAlpha = 1;
     ctx.drawImage(goatImg, this.x, this.y, this.height, this.width);
-
-    // ctx.beginPath();
-    // ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    // // ctx.fillStyle = 'black';
-    // ctx.fillStyle=imgFill;
-    // ctx.fill();
-    // // ctx.strokeStyle = this.stroke
-    // // ctx.lineWidth = 2
-    // // ctx.stroke()
-    // // ctx.shadowBlur = 20
-    // // ctx.shadowColor = 'white'
-    // // ctx.globalAlpha = 0.9
-    // ctx.closePath();
   }
 
   this.update = function(){
     // console.log('updating');
-    let hit = branches.filter(x => this.x < (x.x2 + goatWidth/2) && this.x > (x.x1 - goatWidth/2) && this.y > (x.y - 5 - goatWidth - 3))
+    let hit = branches.filter(x => this.x < (x.x2 + goatWidth) && this.x > (x.x1 - goatWidth) && this.y > (x.y - 5 - goatWidth - 3))
 
     if(hit.length > 0){
       this.y = hit[0].y - goatWidth - 3;
-      staticGoatsArr.push(this);
-      console.log('our static goats', staticGoatsArr);
-      generateGoatStartingCoords();
-      goat = new Goat (startingX, startingY, 0, 4, goatWidth);
+      if(staticGoatsArr.length < 5){
+        staticGoatsArr.push(this);
+        console.log('our static goats', staticGoatsArr.length);
+        generateGoatStartingCoords();
+        goat = new Goat (startingX, startingY, 0, 4, 100, 100);
+      }
     } else {
       this.y += this.dy;
     }
@@ -153,17 +130,17 @@ function Goat (x, y, dx, dy, height, width) {
 function moveGoat(e){
   switch(e.keyCode){
     case 39:
-      // console.log('move RIGHT, goat!');
+      console.log('move RIGHT, goat!');
       goat.x += 15;
       // goat.dy = 1;
       break;
     case 37:
-      // console.log('move LEFT, goat!!');
+      console.log('move LEFT, goat!!');
       goat.x -= 15;
       // goat.dy = 1;
       break;
     case 40:
-      // console.log('move down, goat!');
+      console.log('move down, goat!');
       goat.y += 15;
       break;
   }
