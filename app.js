2c99d289a6c2ded2c838b89222c672c3539a6b02
Oksana Harris <oksana.harris@gmail.com>
2019-01-26 12:55:00 -1000

    don't push that red button

--- app.js
+++ app.js
@@ -6,10 +6,16 @@ let ctx = canvas.getContext('2d');
 
 window.addEventListener('keydown', moveGoat, false);
 
+let bigRedButton = document.getElementById('bigRedButton');
+bigRedButton.addEventListener('click', turnGravityOff);
+
+let gravity = true;
+
 let staticGoatsArr =[];
 let goat;
 
 let audio = new Audio('/assets/goatscream.mp3');
+let goatsBleating = new Audio('/assets/goatsbleating.mp3');
 
 // const colors = ['#4deeea', '#74ee15', '#ffe700', '#f000ff', '#001eff', '#ff0303', '#8400ff', '#00fff6', '#0028ff', '#00ff28', '#ffa300', '#cf0060', '#ff00ff', '#13a8fe', '#4e87a4', '#b0d5ce', '#fff1e4', '#fa86ab', '#ee2889','#7b297d', '#e87888', '#eae8e5', '#b1185a','#c351a2', '#efa9df', '#f3cff1']
 
@@ -56,6 +62,10 @@ let audio = new Audio('/assets/goatscream.mp3');
 function randomIntFromRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min)
   console.log('running random INt');
+}
+
+function randomFloatFromRange(min, max) {
+    return Math.random() * (max - min + 1) + min
 }
 
 let goatWidth = 100;
@@ -107,6 +117,9 @@ let goatImg = new Image(100,100);
 goatImg.src = '/assets/goat.png';
 canvas.appendChild(goatImg);
 
+let TO_RADIANS = Math.PI/180;
+let degrees = 10;
+
 function Goat (x, y, dx, dy, height, width) {
   console.log('running Goat');
   this.x = x;
@@ -115,20 +128,27 @@ function Goat (x, y, dx, dy, height, width) {
   this.dy = dy;
   this.height = height;
   this.width = width;
-
+  this.dxgravity = randomFloatFromRange(-1.5,1.5);
+  this.dygravity = -randomFloatFromRange(0.5,1);
 
   this.draw = function(){
     // console.log('drawing');
-
-
-
     // let goatX = this.x;
     // let goatY = this.y;
     // let goatH = 100;
     // let goatW = 100;
 
     ctx.globalAlpha = 1;
-    ctx.drawImage(goatImg, this.x, this.y, this.height, this.width);
+    if (gravity == false){
+      ctx.save();
+      ctx.translate((this.x + 50), (this.y + 50));
+      ctx.rotate(degrees * TO_RADIANS);
+      ctx.drawImage(goatImg, -50, -50, 100, 100);
+      ctx.restore();
+      degrees+=0.5;
+    } else {
+      ctx.drawImage(goatImg, this.x, this.y, this.height, this.width);
+    }
 
     // ctx.beginPath();
     // ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
@@ -147,31 +167,36 @@ function Goat (x, y, dx, dy, height, width) {
   this.update = function(){
     // console.log('updating');
 
-    if(this.x < (600 + goatWidth) && this.x > (100 - goatWidth) && this.y > (300 - 5 - goatWidth - 3)){
-      this.y = 300 - goatWidth - 3;
-      if(staticGoatsArr.length < 5){
-        staticGoatsArr.push(this);
-        console.log('our static goats', staticGoatsArr.length);
+    if(gravity == true){
+      if(this.x < (600 + goatWidth) && this.x > (100 - goatWidth) && this.y > (300 - 5 - goatWidth - 3)){
+        this.y = 300 - goatWidth - 3;
+        if(staticGoatsArr.length < 5){
+          staticGoatsArr.push(this);
+          console.log('our static goats', staticGoatsArr.length);
+          generateGoatStartingCoords();
+          goat = new Goat (startingX, startingY, 0, 4, 100, 100);
+        }
+      } else {
+        this.y += this.dy;
+      }
+
+      if(this.y - this.height > canvas.height){
+        splode = new Splode(this.x, -10, 100, 100);
+        splodeCounter = 0;
+        displaySplode = true;
+
+        // splode.active = true;
+        // splode.draw();
+
         generateGoatStartingCoords();
-        goat = new Goat (startingX, startingY, 0, 4, 100, 100);
+        goat.x = startingX;
+        goat.y = startingY;
+        audio.play();
       }
     } else {
-      this.y += this.dy;
-    }
+      this.x += this.dxgravity;
+      this.y += this.dygravity;
 
-    if(this.y - this.height > canvas.height){
-      splode = new Splode(this.x, -10, 100, 100);
-      splodeCounter = 0;
-      displaySplode = true;
-
-
-      // splode.active = true;
-      // splode.draw();
-
-      generateGoatStartingCoords();
-      goat.x = startingX;
-      goat.y = startingY;
-      // audio.play();
     }
 
     this.draw();
@@ -198,6 +223,24 @@ function moveGoat(e){
   e.preventDefault();
 }
 
+function turnGravityOff(){
+  if (gravity == true){
+    gravity = false;
+    audio.currentTime = 5;
+    goatsBleating.play();
+
+    setTimeout(function(){
+      goatsBleating.pause();
+    }, 6000);
+  }
+  // else {
+  //   gravity = true;
+  // }
+
+  console.log('gravity ', gravity);
+
+}
+
 function animate(){
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -211,7 +254,11 @@ function animate(){
   ctx.stroke();
 
   for(var i = 0; i < staticGoatsArr.length; i++){
-    staticGoatsArr[i].draw();
+    if (gravity == true){
+      staticGoatsArr[i].draw();
+    } else {
+      staticGoatsArr[i].update();
+    }
   }
 
   goat.update();
