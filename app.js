b7288e8111284deaa0cb3f9ecf23a25af6c3f636
Sarah Yamashige <sarahyamashige@Sarahs-MacBook-Pro.local>
2019-01-25 20:59:57 -1000

    added in test goat image

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ canvas.height = window.innerHeight;
 
 let ctx = canvas.getContext('2d');
 
+// ctx.moveTo(0,0);
+// ctx.lineTo(200, 100);
+// ctx.stroke();
+
+// ctx.beginPath();
+// ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+// ctx.stroke();
+
+let img = document.getElementById("goat");
+canvas.appendChild(img);
+ctx.drawImage(img, 100, 100);
